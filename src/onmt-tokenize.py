1ebbbdd07be69c06d66a26f14608e1387ded8d95
jmcrego <jmcrego@gmail.com>
2019-01-11 10:08:49 +0100

    tokenizer options

--- src/onmt-tokenize.py
+++ src/onmt-tokenize.py
@@ -4,13 +4,37 @@ import sys
 import json
 from tokenizer import build_tokenizer
 
-### read tokenizer
-tok = {'mode': 'conservative', 'vocabulary': ''}
-if len(sys.argv) > 1:
-    with open(sys.argv[1]) as jsonfile: 
-        tok = json.load(jsonfile)
+fjson = None
+tokopts = {'mode': 'aggressive', 'vocabulary': ''}
+usage = """usage: {}
+   -json       FILE : json file containing tokenization options (mode, vocabulary, ...)
+   -mode       MODE : tokenization mode: aggressive, conservative [aggressive]
+   -vocabulary FILE : vocabulary file []
+   -h               : this message
+""".format(sys.argv.pop(0))
 
-tokenizer = build_tokenizer(tok)
+while len(sys.argv):
+    tok = sys.argv.pop(0)
+    if tok=="-json" and len(sys.argv):
+        fjson = sys.argv.pop(0)
+    elif tok=="-mode" and len(sys.argv):
+		tokopts['mode'] = sys.argv.pop(0)
+    elif tok=="-vocabulary" and len(sys.argv):
+		tokopts['vocabulary'] = sys.argv.pop(0)
+    elif tok=="-h":
+		sys.stderr.write("{}".format(usage))
+		sys.exit()
+    else:
+    	sys.stderr.write('error: unparsed {} option\n'.format(tok))
+    	sys.stderr.write("{}".format(usage))
+    	sys.exit()
+
+### read tokenizer options from json file
+if fjson is not None:
+    with open(fjson) as jsonfile: 
+        tokopts = json.load(jsonfile)
+
+tokenizer = build_tokenizer(tokopts)
 
 for line in sys.stdin:
     line, _ = tokenizer.tokenize(str(line.strip('\n')))
