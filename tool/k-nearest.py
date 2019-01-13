2d21daa4d0bd426ccaaf1e93acf519836efe62db
jmcrego <jmcrego@gmail.com>
2019-01-13 17:06:47 +0100

    add column control

--- tool/k-nearest.py
+++ tool/k-nearest.py
@@ -5,13 +5,17 @@ import numpy as np
 from collections import defaultdict
 
 f = None
+c_emb = 0
+c_txt = None
 K = 1
 s = 0.0
 usage = """usage: {} -f FILE [-k INT] [-s FLOAT] [-h] < SENTENCES
-   -f  FILE : file with sentences and their corresponding vector representations
-   -k   INT : show the k nearest sentences [1]
-   -s FLOAT : minimum similarity to consider two sentences near [0.0]
-   -h       : this help
+   -f    FILE : file with sentences and their corresponding vector representations
+   -c_emb INT : column containing vector representations (starting by 1) [2]
+   -c_txt INT : column containing textual sentences (starting by 1) []
+   -k     INT : show the k nearest sentences [1]
+   -s   FLOAT : minimum similarity to consider two sentences near [0.0]
+   -h         : this help
 This scripts finds in file -f the -k nearest sentences to each sentence in 
 SENTENCES and with a similarity score lower than -s. Similarity is computed 
 as the cosine distance of the vectors representing each sentence pair.""".format(sys.argv.pop(0))
@@ -22,6 +26,10 @@ while len(sys.argv):
         f = sys.argv.pop(0)
     elif (tok=="-k" and len(sys.argv)):
         K = int(sys.argv.pop(0))
+    elif (tok=="-c_emb" and len(sys.argv)):
+        c_emb = int(sys.argv.pop(0))-1
+    elif (tok=="-c_txt" and len(sys.argv)):
+        c_txt = int(sys.argv.pop(0))-1
     elif (tok=="-s" and len(sys.argv)):
         s = float(sys.argv.pop(0))
     elif (tok=="-h"):
@@ -35,31 +43,47 @@ if f is None:
     sys.stderr.write('error: missing -f option\n{}\n'.format(usage))
     sys.exit()
 
+### read tst sentences
 VEC = []
 TXT = []
 with open(f) as f:
+    nline = 0
     for line in f:
-        txt, vtxt = line.rstrip('\n').split('\t')
-        vec = map(float, vtxt.strip().split(' '))
+        tok = line.rstrip('\n').split('\t')
+        vec = map(float, tok[c_emb].strip().split(' '))
         vec = vec/np.linalg.norm(vec)
+        if c_txt is not None: txt = tok[c_txt]:
+        else: txt = str(nline)
         VEC.append(vec)
         TXT.append(txt)
+        nline += 1
 
+### read trn sentences
 nline = 0
 for line in sys.stdin:
-    nline += 1
-    txt, vtxt = line.rstrip('\n').split('\t')
-    vec = map(float, vtxt.strip().split(' '))
+    tok = line.rstrip('\n').split('\t')
+    vec = map(float, tok[c_emb].strip().split(' '))
     vec = vec/np.linalg.norm(vec)
+    if c_txt is not None: txt = tok[c_txt]:
+    else: txt = str(nline)
+
+    ### find proximity to all tst sentences
     res = defaultdict(float)
     for i in range(len(VEC)):
         sim = np.sum(VEC[i] * vec) 
         res[i] = sim
+
+    ### output the tst sentences closest to this trn sentence
     print ("{}\t{}".format(nline, txt))
     k = 0
     for i in sorted(res, key=res.get, reverse=True):    
         sim = res[i]
         if sim < s: break
-        print("\t{:.4f}\t{}\t{}".format(sim,i+1,TXT[i]))
+        print("\t{:.4f}\t{}\t{}".format(sim,i,TXT[i]))
         k += 1
         if k == K: break
+
+    nline += 1
+
+
+
