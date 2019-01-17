bc3b82dcf563ad3f5158d7b89f92532188a5a82a
jmcrego <jmcrego@gmail.com>
2019-01-17 14:24:47 +0100

    optimization

--- src/model.py
+++ src/model.py
@@ -297,7 +297,7 @@ class Model():
 
             fd = self.get_feed_dict(src_batch, len_src_batch)
             embed_snt_src_batch, embed_src_batch = self.sess.run([self.embed_snt, self.embed_src], feed_dict=fd)
-            print("embed_src_batch[1] {}".format(embed_src_batch[1]))
+            print("embed_src_batch[1] {}".format(embed_src_batch[0][1]))
 
             if bitext:
                 fd = self.get_feed_dict(self.ref_as_src(ref_batch, len_tgt_batch))
