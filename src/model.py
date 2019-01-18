49103e27bfdefbf3dac518be8b8669276bbe79f8
jmcrego <jmcrego@gmail.com>
2019-01-18 11:03:03 +0100

    bug fix

--- src/model.py
+++ src/model.py
@@ -330,10 +330,10 @@ class Model():
         ### add initial <bos> to ref and increase by 1 len_tgt
         ### it only works if both sides (src/tgt) have been seen by the encoder (sharing vocabularies)
         B, S = np.array(ref).shape
-        bos = np.empty([B,S], dtype=int)
-        print("after empty", bos)
-        bos.fill(self.config.voc_tgt.idx_bos)
-        print("after fill", bos)
+        bos = np.full([B,S], self.config.voc_tgt.idx_bos, dtype=int)
+        print("after full", bos)
+#        bos.fill(self.config.voc_tgt.idx_bos)
+#        print("after fill", bos)
         ref = np.concatenate((bos, ref), axis=1)
         print("after concatenate", bos)
         len_tgt_batch += 1
