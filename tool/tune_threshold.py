d98fc7264dca1c69c48b8922ee6c77955c252ee4
jmcrego <jmcrego@gmail.com>
2019-01-30 21:40:50 +0100

    continue

--- tool/tune_threshold.py
+++ tool/tune_threshold.py
@@ -8,16 +8,16 @@ from collections import defaultdict
 
 def do_parallel(i):
     src, tgt = data[i]
-    print("{}\t{}\t{}".format('P', src, tgt))
+    print("P\t{}\t{}".format(src, tgt))
 
 def do_uneven(i):
     src, tgt = data[i]
     j = (i + random.randint(1, len(data)-1)) % len(data)
     src2, tgt2 = data[j]
     if random.uniform(0, 1) >= 0.5:
-        print("{}\t{}\t{}".format('U', src2, tgt))
+        print("Us\t{}\t{}".format(src2, tgt))
     else:
-        print("{}\t{}\t{}".format('U', src, tgt2))
+        print("Ut\t{}\t{}".format(src, tgt2))
 
 def do_insert(i):
     src, tgt = data[i]
@@ -30,7 +30,7 @@ def do_insert(i):
         SRC = src.split(' ')
         into = random.randint(0,len(SRC)) #[0,n)
         SRC.insert(into," ".join(SRC2[first2:last2+1]))
-        print("{}{}\t{}\t{}".format('Is',last2-first2+1, " ".join(SRC), tgt))
+        print("Is_{}_{}\t{}\t{}".format(into, last2-first2+1, " ".join(SRC), tgt))
     else:
         TGT2 = tgt2.split(' ')
         first2 = random.randint(0,len(TGT2)) #[0,n)
@@ -38,7 +38,7 @@ def do_insert(i):
         TGT = tgt.split(' ')
         into = random.randint(0,len(TGT)) #[0,n)
         TGT.insert(into," ".join(TGT2[first2:last2+1]))   
-        print("{}{}\t{}\t{}".format('It',last2-first2+1, src, " ".join(TGT)))
+        print("It_{}_{}\t{}\t{}".format(into, last2-first2+1, src, " ".join(TGT)))
 
 def do_delete(i):
     src, tgt = data[i]
@@ -47,13 +47,13 @@ def do_delete(i):
         first2 = random.randint(0,len(SRC2)) #[0,n)
         last2 = random.randint(first2,len(SRC2)) #[first,n)
         SRC = SRC2[0:first2-1] + SRC2[last2+1:]
-        print("{}{}\t{}\t{}".format('Ds',last2-first2+1, " ".join(SRC), tgt))
+        print("Ds_{}_{}\t{}\t{}".format(first2,last2, " ".join(SRC), tgt))
     else:
         TGT2 = tgt.split(' ')
         first2 = random.randint(0,len(TGT2)) #[0,n)
         last2 = random.randint(first2,len(TGT2)) #[first,n)
         TGT = TGT2[0:first2-1] + TGT2[last2+1:]
-        print("{}{}\t{}\t{}".format('Dt',last2-first2+1, src, " ".join(TGT)))
+        print("Dt_{}_{}\t{}\t{}".format(first2,last2, src, " ".join(TGT)))
 
 
 do_threshold = False
