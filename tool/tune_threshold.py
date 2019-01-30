bd5a5fb13a6622d16706ad10e3d3475412aa1ce4
jmcrego <jmcrego@gmail.com>
2019-01-30 21:53:11 +0100

    continue

--- tool/tune_threshold.py
+++ tool/tune_threshold.py
@@ -6,8 +6,6 @@ import random
 import numpy as np
 from collections import defaultdict
 
-random.seed(1234)
-
 def do_parallel(i):
     src, tgt = data[i]
     print("P\t{}\t{}".format(src, tgt))
@@ -26,11 +24,16 @@ def do_insert(i):
     j = (i + random.randint(1, len(data)-1)) % len(data)
     src2, tgt2 = data[j]
     if random.uniform(0, 1) >= 0.5:
+        print("src",src)
+        print("src2",src2)
         SRC2 = src2.split(' ')
         first2 = random.randint(0,len(SRC2)) #[0,n)
         last2 = random.randint(first2,len(SRC2)) #[first,n)
+        print("first2",str(first2))
+        print("last2",str(last2))
         SRC = src.split(' ')
         into = random.randint(0,len(SRC)) #[0,n)
+        print("last2",str(into))
         SRC.insert(into," ".join(SRC2[first2:last2+1]))
         print("Is_{}_{}\t{}\t{}".format(into, last2-first2+1, " ".join(SRC), tgt))
     else:
@@ -60,9 +63,11 @@ def do_delete(i):
 
 do_threshold = False
 do_generate = False
+seed = 1234
 usage = """usage: {} [-mode STRING] [-threshold] < INPUT
    -mode   STRING : use p:parallel u:uneven i:insert d:delete to generate sentence pairs
    -threshold     : optimizes similarity threshold
+   -seed      INT : seed [1234]
    -h             : this help
 This script either:
 - Generates src/tgt similar/non-similar translations according to -mode options (previous to tune threshold).
@@ -79,6 +84,8 @@ while len(sys.argv):
         mode = sys.argv.pop(0)
     elif (tok=="-threshold"):
         do_threshold = True
+    elif (tok=="-seed" and len(sys.argv)):
+        seed = int(sys.argv.pop(0))
     elif (tok=="-h"):
         sys.stderr.write("{}\n".format(usage))
         sys.exit()
@@ -86,6 +93,7 @@ while len(sys.argv):
         sys.stderr.write('error: unparsed {} option\n{}\n'.format(tok,usage))
         sys.exit()
 
+random.seed(seed)
 data = []
 for line in sys.stdin:
     (src, tgt) = line.rstrip('\n').split('\t')
