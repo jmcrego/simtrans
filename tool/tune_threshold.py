7863f64b6057061647d087565795db3d453fd494
jmcrego <jmcrego@gmail.com>
2019-01-31 10:45:26 +0100

    continue

--- tool/tune_threshold.py
+++ tool/tune_threshold.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python -u
 
 import sys
-#from random import randint
 import random
 import numpy as np
 from collections import defaultdict
+
+def insert(str, str2):
+    VEC = str.split(' ')
+    VEC2 = str2.split(' ')
+#    print("VEC {}".format(VEC))
+#    print("\tVEC2 {}".format(VEC2))
+    range2 = sorted(np.random.randint(len(VEC2), size=2)) #list of 2 (sorted) integers in the range [0,n)
+#    print("\trange2 {}".format(range2))
+    slice2 = VEC2[range2[0]:range2[1]+1]
+#    print("\tslice2 {}".format(slice2))
+    into1 = np.random.randint(len(VEC)) #[0,n)
+#    print("\tinto1 {}".format(into1))
+    VEC[into1:into1] = slice2 #insert slice2 into VEC at position into
+#    print("\tVEC {}".format(VEC))
+    return " ".join(VEC)
+
+def delete(str):
+    VEC = str.split(' ')
+#    print("VEC {}".format(VEC))
+    range1 = sorted(np.random.randint(len(VEC), size=2)) #list of 2 (sorted) integers in the range [0,n)
+#    print("\trange1 {}".format(range1))
+    del VEC[range1[0]:range1[1]+1]
+#    print("\tVEC {}".format(VEC))
+    return " ".join(VEC)
 
 def do_parallel(i):
     src, tgt = data[i]
@@ -12,55 +35,28 @@ def do_parallel(i):
 
 def do_uneven(i):
     src, tgt = data[i]
-    j = (i + random.randint(1, len(data)-1)) % len(data)
+    j = (i + np.random.randint(1, len(data))) % len(data)
     src2, tgt2 = data[j]
-    if random.uniform(0, 1) >= 0.5:
+    if np.random.uniform(0, 1) >= 0.5:
         print("Us\t{}\t{}".format(src2, tgt))
     else:
         print("Ut\t{}\t{}".format(src, tgt2))
 
 def do_insert(i):
     src, tgt = data[i]
-    j = (i + random.randint(1, len(data)-1)) % len(data)
+    j = (i + np.random.randint(1, len(data)-1)) % len(data)
     src2, tgt2 = data[j]
-    if random.uniform(0, 1) >= 0.5:
-#        print("src",src)
-#        print("src2",src2)
-        SRC2 = src2.split(' ')
-#        print("len(SRC2)",len(SRC2))
-        first2 = random.randint(0,len(SRC2)-1) #[0,n)
-        last2 = random.randint(first2,len(SRC2)-1) #[first,n)
-#        print("first2",str(first2))
-#        print("last2",str(last2))
-        SRC = src.split(' ')
-#        print("len(SRC)",len(SRC))
-        into = random.randint(0,len(SRC)) #[0,n)
-#        print("into",str(into))
-        SRC.insert(into," ".join(SRC2[first2:last2+1]))
-        print("Is_{}_{}\t{}\t{}".format(into, last2-first2+1, " ".join(SRC), tgt))
+    if np.random.uniform(0, 1) >= 0.5:
+        print("Is\t{}\t{}".format(insert(src,src2), tgt))
     else:
-        TGT2 = tgt2.split(' ')
-        first2 = random.randint(0,len(TGT2)-1) #[0,n)
-        last2 = random.randint(first2,len(TGT2)-1) #[first,n)
-        TGT = tgt.split(' ')
-        into = random.randint(0,len(TGT)) #[0,n)
-        TGT.insert(into," ".join(TGT2[first2:last2+1]))   
-        print("It_{}_{}\t{}\t{}".format(into, last2-first2+1, src, " ".join(TGT)))
+        print("It\t{}\t{}".format(src, insert(tgt,tgt2)))
 
 def do_delete(i):
     src, tgt = data[i]
-    if random.uniform(0, 1) >= 0.5:
-        SRC2 = src.split(' ')
-        first2 = random.randint(0,len(SRC2)) #[0,n)
-        last2 = random.randint(first2,len(SRC2)) #[first,n)
-        SRC = SRC2[0:first2-1] + SRC2[last2+1:]
-        print("Ds_{}_{}\t{}\t{}".format(first2,last2, " ".join(SRC), tgt))
+    if np.random.uniform(0, 1) >= 0.5:
+        print("Ds\t{}\t{}".format(delete(src), tgt))
     else:
-        TGT2 = tgt.split(' ')
-        first2 = random.randint(0,len(TGT2)) #[0,n)
-        last2 = random.randint(first2,len(TGT2)) #[first,n)
-        TGT = TGT2[0:first2-1] + TGT2[last2+1:]
-        print("Dt_{}_{}\t{}\t{}".format(first2,last2, src, " ".join(TGT)))
+        print("Dt\t{}\t{}".format(src, delete(tgt)))
 
 
 do_threshold = False
@@ -104,9 +100,9 @@ sys.stderr.write("Read {} entries\n".format(len(data)))
 
 if do_generate:
     for i in range(len(data)):
-        do_parallel(i)
-        do_uneven(i)
-        do_insert(i)
-        do_delete(i)
+        if 'p' in mode: do_parallel(i)
+        if 'u' in mode: do_uneven(i)
+        if 'i' in mode: do_insert(i)
+        if 'd' in mode: do_delete(i)
 
 
