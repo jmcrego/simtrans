6a8d9a57c580fcd4a2146f794df6929b5aae32cd
jmcrego <jmcrego@gmail.com>
2019-02-05 21:26:01 +0100

    change of options and enc/dec loop of layers

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -6,16 +6,14 @@ import six
 
 def build_tokenizer(args):
     """Builds a tokenizer based on user arguments."""
-#    sys.stderr.write('tokenizer args = {}\n'.format(args))
     import pyonmttok
     local_args = {}
     for k, v in six.iteritems(args):
         if isinstance(v, six.string_types):
             local_args[k] = v.encode('utf-8')
-            sys.stderr.write("{} {}\n".format(k,local_args[k]))
         else:
             local_args[k] = v
-            sys.stderr.write("{} {}\n".format(k,local_args[k]))
+    sys.stderr.write('tokenizer args = {}\n'.format(args))
     mode = local_args['mode']
     del local_args['mode']
     del local_args['vocabulary']
