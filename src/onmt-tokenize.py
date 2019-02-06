20ef7a60ce522e66112c36f751e5066c169823be
Josep CREGO <crego@ssaling12.systran.local>
2019-02-06 13:49:41 +0100

    tokenizer

--- src/onmt-tokenize.py
+++ src/onmt-tokenize.py
@@ -2,18 +2,41 @@
 
 import sys
 import json
-from tokenizer import build_tokenizer
+import yaml
+import os
+import six
+import pyonmttok
+
+def build_tokenizer(args):
+    """Builds a tokenizer based on user arguments."""
+    local_args = {}
+    for k, v in six.iteritems(args):
+        if isinstance(v, six.string_types):
+            local_args[k] = v.encode('utf-8')
+        else:
+            local_args[k] = v
+
+    if not 'mode' in local_args:
+        sys.stderr.write('error: missing mode in tokenizer options\n')
+        sys.exit()
+        
+    mode = local_args['mode']
+    del local_args['mode']
+    if 'vocabulary' in local_args: del local_args['vocabulary']
+    return pyonmttok.Tokenizer(mode, **local_args)
+    
 
 fjson = None
 case = False
 tokopts = {'mode': 'aggressive', 'vocabulary': ''}
 usage = """usage: {}
    -json       FILE : json file containing tokenization options (mode, vocabulary, ...)
+   -yaml       FILE : yaml file containing tokenization options (mode, vocabulary, ...)
    -mode       MODE : tokenization mode: aggressive, conservative [aggressive]
-   -bpe_model  FILE : 
-   -joiner_annotate : use joiner annotate
+   -bpe        FILE : bpe codes to apply bpe subtokenization (use -bpe_model_path)
+   -joiner          : use joiner annotate (use -joiner_annotate))
    -lc              : lowercase all data (use -case_feature)
-   -vocabulary FILE : vocabulary file []
+   -vocabulary FILE : vocabulary file
    -h               : this message
 """.format(sys.argv.pop(0))
 
@@ -22,36 +45,43 @@ while len(sys.argv):
     if tok=="-json" and len(sys.argv):
     	fjson = sys.argv.pop(0)
 
+    elif tok=="-yaml" and len(sys.argv):
+    	fyaml = sys.argv.pop(0)
+        
     elif tok=="-lc":
         tokopts['case_feature'] = True
 
-    elif tok=="-joiner_annotate":
-		tokopts['joiner_annotate'] = True
+    elif tok=="-joiner":
+	tokopts['joiner_annotate'] = True
 
     elif tok=="-mode" and len(sys.argv):
-		tokopts['mode'] = sys.argv.pop(0)
+	tokopts['mode'] = sys.argv.pop(0)
 
-    elif tok=="-bpe_model" and len(sys.argv):
-		tokopts['bpe_model_path'] = sys.argv.pop(0)
+    elif tok=="-bpe" and len(sys.argv):
+	tokopts['bpe_model_path'] = sys.argv.pop(0)
 
     elif tok=="-vocabulary" and len(sys.argv):
-  		tokopts['vocabulary'] = sys.argv.pop(0)
+  	tokopts['vocabulary'] = sys.argv.pop(0)
 
     elif tok=="-h":
-		sys.stderr.write("{}".format(usage))
-		sys.exit()
+	sys.stderr.write("{}".format(usage))
+	sys.exit()
+
     else:
     	sys.stderr.write('error: unparsed {} option\n'.format(tok))
     	sys.stderr.write("{}".format(usage))
     	sys.exit()
 
-### read tokenizer options from json file
-if fjson is not None:
+if fjson is not None: ### read tokenizer options from json file
     with open(fjson) as jsonfile: 
         tokopts = json.load(jsonfile)
+elif fyaml is not None: ### read tokenizer options from yaml file
+    with open(fyaml) as yamlfile:
+        tokopts = yaml.load(yamlfile)
 
-tokenizer = build_tokenizer(tokopts)
+sys.stderr.write("tokenizer args = {}\n".format(tokopts))
 
+t = build_tokenizer(tokopts)
 for line in sys.stdin:
-    line, _ = tokenizer.tokenize(str(line.strip('\n')))
+    line, _ = t.tokenize(str(line.strip('\n')))
     print("{}".format(" ".join(line)))
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -7,14 +7,20 @@ import six
 def build_tokenizer(args):
     """Builds a tokenizer based on user arguments."""
     import pyonmttok
+
     local_args = {}
     for k, v in six.iteritems(args):
         if isinstance(v, six.string_types):
             local_args[k] = v.encode('utf-8')
         else:
             local_args[k] = v
-    sys.stderr.write('tokenizer args = {}\n'.format(args))
+
+    if not 'mode' in local_args:
+        sys.stderr.write('error: missing mode in tokenizer options\n')
+        sys.exit()
+
     mode = local_args['mode']
     del local_args['mode']
-    del local_args['vocabulary']
+    if 'vocabulary' in local_args:
+        del local_args['vocabulary']
     return pyonmttok.Tokenizer(mode, **local_args)
